fe4bc3f9a1daefe9d782d3fb956d422287884b66
enniohenrique <ennioh@icloud.com>
2022-05-09 23:35:21 -0400

    Update

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let sliderElement = document.querySelector('#slider')
+let buttonElement = document.querySelector('#button')
+let tamanhoSenha= document.querySelector('#valor')
+let senha= document.querySelector('#senha')
+let containerSenha= document.querySelector('#container-senha')
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0"
+let novaSenha = "";
+
+tamanhoSenha.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    tamanhoSenha.innerHTML = this.value;
+}
+
+function gerarSenha(){
+    let pass = "";
+    for(let i = 0, n = charset.length;i<sliderElement.value; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerSenha.classList.remove("hide")
+    senha.innerHTML = pass;
+    novaSenha = pass
+    
+}
+
+
+function copiarSenha(){
+    alert("Senha copiada com sucesso")
+    navigator.clipboard.writeText(novaSenha);
+}
+
